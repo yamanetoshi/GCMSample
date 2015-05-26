222a00adf1b67af9c8db194604f5dfe52e5a0cf8
YAMANE Toshiaki <yamanetoshi@gmail.com>
2015-05-26 15:50:47 +0900

    initial commit

---
+++ app/src/main/java/com/example/gcmsampleapplication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.gcmsampleapplication;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private GoogleCloudMessaging mGcm;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+
+        mGcm = GoogleCloudMessaging.getInstance(this);
+        registerInBackground();
+    }
+
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (mGcm == null) {
+                        mGcm = GoogleCloudMessaging.getInstance(mContext);
+                    }
+                    String regid = mGcm.register("<Application ID>");
+                    msg = regid;
+                    Log.d(TAG, "Device registered, registration id is " + regid);
+                } catch (Exception e) {
+                    msg = "Error :" + e.getMessage();
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                // send regid to Application Server
+                Log.d(TAG, "argument :" + s);
+            }
+        }.execute(null, null, null);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
